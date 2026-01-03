d7d0c775829b1817135146ccaabacbf6d9c801f1
Rutuparn Pawar <rutuparn.pawar@outlook.com>
2026-01-03 10:26:53 -0500

    Add icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import icon from "../assets/icon.png";
 
 const Header = () => {
-    return (
-      <div className="container-fluid pt-4 pb-2">
-        <h1 className="text-center">Mad Libs Diceware</h1>
-      </div>
-    );
-}
+  return (
+    <div className="container-fluid pt-4 pb-2">
+      <h1 className="text-center">
+        <img
+          src={icon}
+          alt="Icon"
+          style={{ height: "1em", marginRight: "0.5em" }}
+        />
+        Mad Libs Diceware
+      </h1>
+    </div>
+  );
+};
 
 export default Header;

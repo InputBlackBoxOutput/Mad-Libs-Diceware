60577867819247145599a1cde47850743809a0cc
Rutuparn Pawar <rutuparn.pawar@outlook.com>
2026-01-03 10:25:41 -0500

    Add File IO fallback mechanism

--- backend/server.js
+++ backend/server.js
@@ -2,121 +2,158 @@
 // Written by Rutuparn Pawar (InputBlackBoxOutput)
 
 const diceware = require("./diceware.js");
-const express = require('express');
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-require('dotenv').config()
+require("dotenv").config();
+
+// Fallback to file IO if MongoDB is not available
+const fs = require("fs");
+const path = require("path");
+
+function wordlistFileIO(filePath) {
+  try {
+    const data = fs.readFileSync(
+      path.resolve(__dirname, "assets", filePath),
+      "utf8"
+    );
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(`Error reading file: ${err}`);
+    throw err;
+  }
+}
 
 const app = express();
 const port = process.env.PORT || 5000;
 const corsOptions = {
-	"origin": "*",
-	"methods": "GET",
-	"preflightContinue": false,
-	"optionsSuccessStatus": 200
-}
+  origin: "*",
+  methods: "GET",
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ff8np.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get("/", (req, res) => {
-	res.redirect('/mad-libs-diceware');
-})
-
-app.get("/eff-diceware", (req, res) => {
-	const nwords = req.query.nwords;
-
-	MongoClient.connect(uri, function (err, db) {
-		if (err) throw err;
-
-		var dbo = db.db("Mad-Libs-Diceware");
-		dbo.collection("eff-wordlist").find({}).toArray(function (err, result) {
-			if (err) throw err;
-			db.close();
-
-			wordlist = {};
-			result.map(entry => {
-				wordlist[entry.index] = entry.word;
-			})
-
-			if (nwords == undefined)
-				res.send(diceware.effDiceware(wordlistEFF = wordlist, nWords = 6));
-			else {
-				if (nwords < 4 || nwords > 7)
-					res.status(422).send({ error: 'Invalid number of words' });
-				else
-					res.send(diceware.effDiceware(wordlistEFF = wordlist, nWords = nwords));
-			}
-		});
-	});
-
-
+  res.redirect("/mad-libs-diceware");
 });
 
-app.get("/mad-libs-diceware", (req, res) => {
-	MongoClient.connect(uri, function (err, db) {
-		if (err) throw err;
+app.get("/eff-diceware", async (req, res) => {
+  const nwords = req.query.nwords;
+  let wordlist = {};
 
-		var dbo = db.db("Mad-Libs-Diceware");
-		dbo.collection("mad-libs-wordlist").find({}).toArray(function (err, result) {
-			if (err) throw err;
-			db.close();
+  try {
+    const client = await MongoClient.connect(uri);
+    const dbo = client.db("Mad-Libs-Diceware");
+    const result = await dbo.collection("eff-wordlist").find({}).toArray();
 
-			wordlist = {
-				"adverbs": result[0]["list"],
-				"adjectives": result[1]["list"],
-				"nouns": result[2]["list"]
-			};
+    result.forEach((entry) => {
+      wordlist[entry.index] = entry.word;
+    });
 
-			res.send(diceware.madLibsDiceware(wordlistMadlibs = wordlist));
+    client.close();
+  } catch (err) {
+    console.log("MongoDB connection error: Using file IO");
+    wordlist = wordlistFileIO("wordlist-eff.json");
+  }
 
-		});
-	});
-})
-
-app.get("/eff-wordlist", (req, res) => {
-	MongoClient.connect(uri, function (err, db) {
-		if (err) throw err;
-
-		var dbo = db.db("Mad-Libs-Diceware");
-		dbo.collection("eff-wordlist").find({}).toArray(function (err, result) {
-			if (err) throw err;
-			db.close();
-
-			wordlist = {};
-			result.map(entry => {
-				wordlist[entry.index] = entry.word;
-			})
-			res.send(wordlist);
-		});
-	});
+  if (nwords == undefined)
+    res.send(diceware.effDiceware((wordlistEFF = wordlist), (nWords = 6)));
+  else {
+    if (nwords < 4 || nwords > 7)
+      res.status(422).send({ error: "Invalid number of words" });
+    else
+      res.send(
+        diceware.effDiceware((wordlistEFF = wordlist), (nWords = nwords))
+      );
+  }
 });
 
-app.get("/mad-libs-wordlist", (req, res) => {
-	MongoClient.connect(uri, function (err, db) {
-		if (err) throw err;
+app.get("/mad-libs-diceware", async (req, res) => {
+  let wordlist = {};
 
-		var dbo = db.db("Mad-Libs-Diceware");
-		dbo.collection("mad-libs-wordlist").find({}).toArray(function (err, result) {
-			if (err) throw err;
-			db.close();
+  try {
+    const client = await MongoClient.connect(uri);
+    const dbo = client.db("Mad-Libs-Diceware");
+    const result = await dbo.collection("mad-libs-wordlist").find({}).toArray();
 
-			wordlist = {
-				"adverbs": result[0]["list"],
-				"adjectives": result[1]["list"],
-				"nouns": result[2]["list"]
-			};
-			res.send(wordlist);
-		});
-	});
-})
+    wordlist = {
+      adverbs: result[0]["list"],
+      adjectives: result[1]["list"],
+      nouns: result[2]["list"],
+    };
+
+    client.close();
+  } catch (err) {
+    console.log("MongoDB connection error: Using file IO");
+    const wordlists = wordlistFileIO("wordlist-mad-libs.json");
+    wordlist = {
+      adverbs: wordlists["adverbs"],
+      adjectives: wordlists["adjectives"],
+      nouns: wordlists["nouns"],
+    };
+  }
+
+  res.send(diceware.madLibsDiceware((wordlistMadlibs = wordlist)));
+});
+
+app.get("/eff-wordlist", async (req, res) => {
+  let wordlist = {};
+
+  try {
+    const client = await MongoClient.connect(uri);
+    const dbo = client.db("Mad-Libs-Diceware");
+    const result = await dbo.collection("eff-wordlist").find({}).toArray();
+
+    result.forEach((entry) => {
+      wordlist[entry.index] = entry.word;
+    });
+
+    client.close();
+  } catch (err) {
+    console.log("MongoDB connection error: Using file IO");
+    wordlist = wordlistFileIO("wordlist-eff.json");
+  }
+
+  res.send(wordlist);
+});
+
+app.get("/mad-libs-wordlist", async (req, res) => {
+  let wordlist = {};
+
+  try {
+    const client = await MongoClient.connect(uri);
+    const dbo = client.db("Mad-Libs-Diceware");
+    const result = await dbo.collection("mad-libs-wordlist").find({}).toArray();
+
+    wordlist = {
+      adverbs: result[0]["list"],
+      adjectives: result[1]["list"],
+      nouns: result[2]["list"],
+    };
+
+    client.close();
+  } catch (err) {
+    console.log("MongoDB connection error: Using file IO");
+    const wordlists = wordlistFileIO("wordlist-mad-libs.json");
+    wordlist = {
+      adverbs: wordlists["adverbs"],
+      adjectives: wordlists["adjectives"],
+      nouns: wordlists["nouns"],
+    };
+  }
+
+  res.send(wordlist);
+});
 
 app.get("*", (req, res) => {
-	res.status(404).send({ error: 'Invalid route', redirect: '/mad-libs-diceware' })
-})
+  res
+    .status(404)
+    .send({ error: "Invalid route", redirect: "/mad-libs-diceware" });
+});
 
-app.listen(port, () =>
-	console.log(`Listening on port ${port}!`),
-);
+app.listen(port, () => console.log(`Listening on port ${port}!`));
